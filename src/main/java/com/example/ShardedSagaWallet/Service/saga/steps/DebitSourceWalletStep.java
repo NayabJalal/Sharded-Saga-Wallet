1df39789ab1e1518336f2f596bc0642992deaafa
NayabJalal <jmd341602@gmail.com>
2026-05-23 05:16:51 +0530

    Added Debit wallet Saga Step

---
+++ src/main/java/com/example/ShardedSagaWallet/Service/saga/steps/DebitSourceWalletStep.java
@@ -0,0 +1,72 @@
+package com.example.ShardedSagaWallet.Service.saga.steps;
+
+import com.example.ShardedSagaWallet.Service.saga.SagaContext;
+import com.example.ShardedSagaWallet.Service.saga.SagaStep;
+import com.example.ShardedSagaWallet.entities.Wallet;
+import com.example.ShardedSagaWallet.repository.WalletRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class DebitSourceWalletStep implements SagaStep {
+
+    private final WalletRepository walletRepository;
+
+    @Override
+    @Transactional
+    public boolean execute(SagaContext context) {
+        Long fromWalletId = context.getLong("fromWalletId");
+        BigDecimal amount = context.getBigDecimal("amount");
+
+        log.info("Debiting source wallet with id: {} for amount: {}", fromWalletId, amount);
+
+        Wallet wallet = walletRepository.findByIdWithLock(fromWalletId)
+                .orElseThrow(() -> new IllegalArgumentException("Source wallet not found with id: " + fromWalletId));
+
+        log.info("Wallet fetched with balance: {}", wallet.getBalance());
+        context.put("originalSourceWalletBalance", wallet.getBalance());
+
+        wallet.debit(amount);
+        walletRepository.save(wallet);
+
+        log.info("Wallet saved with new balance: {}", wallet.getBalance());
+        context.put("sourceWalletBalanceAfterDebit", wallet.getBalance());
+        log.info("Debit source wallet step executed successfully");
+
+        return true;
+    }
+
+    @Override
+    @Transactional
+    public boolean compensate(SagaContext context) {
+        Long fromWalletId = context.getLong("fromWalletId");
+        BigDecimal amount = context.getBigDecimal("amount");
+
+        log.info("Compensating debit of source wallet with id: {} for amount: {}", fromWalletId, amount);
+        Wallet wallet = walletRepository.findByIdWithLock(fromWalletId)
+                .orElseThrow(() -> new IllegalArgumentException("Source wallet not found with id: " + fromWalletId));
+
+        log.info("Source wallet fetched with balance: {}", wallet.getBalance());
+        context.put("sourceWalletBalanceBeforeCreditCompensation", wallet.getBalance());
+
+        wallet.credit(amount);
+        walletRepository.save(wallet);
+
+        log.info("Source wallet saved with balance: {}", wallet.getBalance());
+        context.put("sourceWalletBalanceAfterCreditCompensation", wallet.getBalance());
+        log.info("Compensating source wallet step executed successfully");
+
+        return true;
+    }
+
+    @Override
+    public String getStepName() {
+        return "DebitSourceWalletStep";
+    }
+}
